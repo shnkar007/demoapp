cd6cfca8f4cc08579156b95d6d29f485ccdc71ac
Sankararaman Vedagiri <shnkar007@hotmail.com>
2023-07-11 23:22:27 +0530

    Update Demo.java
    try adding

--- src/com/altius/demoapp/Demo.java
+++ src/com/altius/demoapp/Demo.java
@@ -2,6 +2,7 @@
   public class Demo{
 	     public static void main(String... args){
 			  System.out.println("SayHello"+"professors");
+			  System.out.println("Something is wrong");
 		 }
   }
   
