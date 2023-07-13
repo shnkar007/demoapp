723cc440248fc24b1c62a710428a50c47205e7a5
Sankararaman Vedagiri <shnkar007@hotmail.com>
2023-07-13 17:50:50 +0530

    new merging

--- src/com/altius/demoapp/Demo.java
+++ src/com/altius/demoapp/Demo.java
@@ -3,7 +3,7 @@
 	     public static void main(String... args){
 			  System.out.println("SayHello"+"professors");
 			  System.out.println("new diff");
-			  System.out.println("new branch created");
+			  System.out.println("new branch created and merged with master");
 		 }
   }
   
